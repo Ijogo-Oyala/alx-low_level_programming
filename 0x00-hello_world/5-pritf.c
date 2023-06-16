2d0ce24ca598a87ff243d2eb1d083b078ac21a2c
Gift <ubekegift@gmai.com>
2023-06-16 05:55:14 -0700

    hello

---
+++ 0x00-hello_world/5-pritf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * return: (0) (success)
+ */
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
+
